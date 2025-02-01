6d1b2192166983b76f263d5163ad61b1fdfba4a0
Devitor0 <devitormoreira@gmail.com>
2025-02-01 16:10:51 -0600

    Organizando os arquivos de csrf e validação

--- CLIENT/src/templates/Account/LogIn/index.jsx
+++ CLIENT/src/templates/Account/LogIn/index.jsx
@@ -4,133 +4,75 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import icons from '../../../media/icons/fontawesome';
 import image from '../../../utils/imageManager/imageManager';
 import Form from '../../../components/Account/form';
-
+import { fetchCsrfToken } from '../../../utils/csrf/csurfValidation';
+import {
+  validateFields,
+  showErrorMessage,
+} from '../../../utils/formValidations/formValidation';
 import styles from '../../../styles/scss/Account/account.module.scss';
-
-export const showErrorMessage = (message) => {
-  const form = document.querySelector('#form');
-  if (!form) return;
-
-  const existingMessage = Array.from(form.children).find((child) =>
-    child.classList.contains(styles.errorMessage),
-  );
-
-  if (existingMessage) existingMessage.remove();
-
-  const messageDiv = document.createElement('div');
-  messageDiv.classList.add(styles.errorMessage);
-  const messageText = document.createElement('p');
-  messageText.textContent = message;
-
-  messageDiv.appendChild(messageText);
-  form.appendChild(messageDiv);
-};
-
-export const emailIsValid = (email) => {
-  if (!email || email.indexOf('@') === -1 || email.indexOf('.') === -1) {
-    showErrorMessage('Endereço de email inválido');
-    return false;
-  }
-  return true;
-};
-
-export const passwordIsValid = (password) => {
-  if (password && password.length > 25) {
-    showErrorMessage('Senha muito longa');
-    return false;
-  }
-  return true;
-};
-
-export const handleSubmit = async (event, csrfToken, navigate) => {
-  event.preventDefault();
-  const email = event.target.email.value;
-  const password = event.target.senha.value;
-
-  if (!email || !password) {
-    showErrorMessage('Preencha todos os campos!');
-    return;
-  }
-
-  if (!emailIsValid(email) || !passwordIsValid(password)) {
-    return;
-  }
-
-  if (email.length > 35 || password.length > 25) {
-    showErrorMessage('Campos excedem o tamanho máximo!');
-    return;
-  }
-
-  if (!csrfToken) {
-    showErrorMessage('CSRF Token não encontrado!');
-    return;
-  }
-
-  console.log('Token CSRF sendo enviado no cabeçalho:', csrfToken);
-
-  try {
-    const response = await fetch('/api/validate-credentials', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': csrfToken,
-      },
-      credentials: 'include',
-      body: JSON.stringify({ username: email, password: password }),
-    });
-
-    if (!response.ok) {
-      const error = await response.json();
-      showErrorMessage(error.message || 'Erro ao realizar login.');
-      return;
-    }
-
-    const result = await response.json();
-    if (result.redirectUrl) {
-      navigate(result.redirectUrl);
-    }
-  } catch (error) {
-    console.error('Erro ao enviar a requisição:', error);
-    showErrorMessage('Erro interno. Tente novamente mais tarde.');
-  }
-};
 
 export const Login = () => {
   const [csrfToken, setCsrfToken] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchCsrfToken = async () => {
-      try {
-        const response = await fetch('/csrf-token', {
-          method: 'GET',
-          credentials: 'include',
-        });
-
-        if (!response.ok) {
-          console.error('Erro ao buscar token CSRF:', response.statusText);
-          return;
-        }
-
-        const data = await response.json();
-        setCsrfToken(data.csrfToken);
-        console.log('Token CSRF recebido:', data.csrfToken);
-      } catch (error) {
-        console.error('Erro ao buscar token CSRF:', error);
+    const getCsrfToken = async () => {
+      const token = await fetchCsrfToken();
+      if (token) {
+        setCsrfToken(token);
       }
     };
 
-    fetchCsrfToken();
+    getCsrfToken();
   }, []);
 
   const internalHandleSubmit = async (event) => {
-    await handleSubmit(event, csrfToken, navigate);
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.senha.value;
+
+    if (!validateFields(email, password, styles)) {
+      return;
+    }
+
+    if (!csrfToken) {
+      showErrorMessage('CSRF Token não encontrado!', styles);
+      return;
+    }
+
+    console.log('Token CSRF sendo enviado no cabeçalho:', csrfToken);
+
+    try {
+      const response = await fetch('/api/validate-credentials', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': csrfToken,
+        },
+        credentials: 'include',
+        body: JSON.stringify({ username: email, password: password }),
+      });
+
+      if (!response.ok) {
+        const error = await response.json();
+        showErrorMessage(error.message || 'Erro ao realizar login.', styles);
+        return;
+      }
+
+      const result = await response.json();
+      if (result.redirectUrl) {
+        navigate(result.redirectUrl);
+      }
+    } catch (error) {
+      console.error('Erro ao enviar a requisição:', error);
+      showErrorMessage('Erro interno. Tente novamente mais tarde.', styles);
+    }
   };
 
   return (
     <Form
       title="Login"
-      linkText="Entrar/"
+      linkText="Registrar/"
       route="/cadastrar"
       emailPlaceholder="Digite seu email"
       passwordPlaceholder="Digite sua senha"
--- CLIENT/src/templates/Account/SignUp/index.jsx
+++ CLIENT/src/templates/Account/SignUp/index.jsx
@@ -1,18 +1,85 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import icons from '../../../media/icons/fontawesome';
 import image from '../../../utils/imageManager/imageManager';
 import Form from '../../../components/Account/form';
+import { fetchCsrfToken } from '../../../utils/csrf/csurfValidation';
+import {
+  validateFields,
+  showErrorMessage,
+} from '../../../utils/formValidations/formValidation';
+import styles from '../../../styles/scss/Account/account.module.scss';
 
 const SignUp = () => {
+  const [csrfToken, setCsrfToken] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const token = await fetchCsrfToken();
+      if (token) {
+        setCsrfToken(token);
+      }
+    };
+
+    getCsrfToken();
+  }, []);
+
+  const internalHandleSubmit = async (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.senha.value;
+    const username = event.target.username.value;
+
+    if (!validateFields(email, password, styles)) {
+      return;
+    }
+
+    if (!csrfToken) {
+      showErrorMessage('CSRF Token não encontrado!', styles);
+      return;
+    }
+
+    console.log('Token CSRF sendo enviado no cabeçalho:', csrfToken);
+
+    try {
+      const response = await fetch('/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': csrfToken,
+        },
+        credentials: 'include',
+        body: JSON.stringify({ username, email, password }),
+      });
+
+      if (!response.ok) {
+        const error = await response.json();
+        showErrorMessage(error.message || 'Erro ao realizar cadastro.', styles);
+        return;
+      }
+
+      const result = await response.json();
+      if (result.redirectUrl) {
+        navigate(result.redirectUrl);
+      }
+    } catch (error) {
+      console.error('Erro ao enviar a requisição:', error);
+      showErrorMessage('Erro interno. Tente novamente mais tarde.', styles);
+    }
+  };
+
   return (
     <Form
-      title="Entrar"
+      title="Registrar"
       linkText="Login/"
       route="/entrar"
       emailPlaceholder="Digite seu email"
       passwordPlaceholder="Digite sua senha"
       forgetPasswordText="Esqueceu a senha ?"
       submitButtonText="Cadastrar-se"
+      internalHandleSubmit={internalHandleSubmit}
       icons={{
         envelop: (
           <FontAwesomeIcon icon={icons.envelop} data-testid="svg-inline--fa" />
